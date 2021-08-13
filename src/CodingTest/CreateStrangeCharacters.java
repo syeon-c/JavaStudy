87d22f51f0a352b3bafbff42d7f59ba441cf71b6
최서연 <gkdltjdus@gmail.com>
2021-08-13 19:20:12 +0900

    2021.08.13 코테스터디 프로그래머스/level1/이상한 문자 만들기

---
+++ src/CodingTest/CreateStrangeCharacters.java
@@ -0,0 +1,30 @@
+package CodingTest;
+
+import java.util.Locale;
+
+public class CreateStrangeCharacters {
+    public static void main(String[] args) {
+        String s = "try hello world";
+        System.out.println(Solution(s));
+
+    }
+
+    public static String Solution(String s) {
+        String answer = "";
+        String[] sList = s.split(" ", -1);
+
+        for(int i = 0; i < sList.length; i++) {
+            String[] tmp = sList[i].split("");
+            for(int j = 0; j < sList[i].length(); j++) {
+                if(j % 2 == 0) {
+                    answer += tmp[j].toUpperCase();
+                } else {
+                    answer += tmp[j].toLowerCase();
+                }
+            }
+            answer += " ";
+        }
+
+        return answer.substring(0,answer.length()-1);
+    }
+}
