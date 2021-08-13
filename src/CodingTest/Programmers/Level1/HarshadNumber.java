6f19f2f8dbbde2d85ad0e362b7a0268343d56711
최서연 <gkdltjdus@gmail.com>
2021-08-13 19:34:51 +0900

    2021.08.13 코테스터디 프로그래머스/level1/하샤드 수

---
+++ src/CodingTest/Programmers/Level1/HarshadNumber.java
@@ -0,0 +1,22 @@
+package CodingTest.Programmers.Level1;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        int arr = 10;
+        System.out.println(Solution(arr));
+    }
+    public static boolean Solution(int x) {
+        int sum = 0;
+        int n = x;
+
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        if(x % sum != 0)
+            return false;
+
+        return true;
+    }
+}
