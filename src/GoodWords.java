e903df5f06c3691e0dbe1fb947418b0c53391a35
최서연 <gkdltjdus@gmail.com>
2021-09-03 15:29:56 +0900

    2021.09.03/문자열 활용/좋은 단어

---
+++ src/GoodWords.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class GoodWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        int answer = 0;
+
+        for(int i = 0; i < n; i++) {
+            String word = sc.nextLine();
+            String a = "AA";
+            String b = "BB";
+
+            while (true) {
+                if(!word.contains(a) && !word.contains(b)) break;
+                if (word.contains(a)) {
+                    word = word.replaceAll(a, "");
+                }
+
+                if (word.contains(b)) {
+                    word = word.replaceAll(b, "");
+                }
+            }
+            if(word.length() == 0) answer++;
+        }
+
+        System.out.println(answer);
+    }
+}
