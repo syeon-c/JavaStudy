13c3b65937865ee25c1a32d26daa1edfdef8fe62
최서연 <gkdltjdus@gmail.com>
2021-09-07 00:03:45 +0900

    2021.09.06/BOJ/기업 알고리즘 테스트 유형/N과 M(15649)

---
+++ src/BackTracking/NM1_15649.java
@@ -0,0 +1,38 @@
+package BackTracking;
+
+import java.util.Scanner;
+
+public class NM1_15649 {
+    private static boolean[] visited;
+    private static int[] arr;
+
+    public static void dfs(int n, int m, int i) {
+        if(i == m) {
+            for(int val : arr)
+                System.out.print(val + " ");
+
+            System.out.println();
+            return;
+        }
+
+        for(int j = 0; j < n; j++) {
+            if(!visited[j]) {
+                visited[j] = true;
+                arr[i] = j + 1;
+                dfs(n, m , i + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        visited = new boolean[n];
+        arr = new int[m];
+
+        dfs(n, m, 0);
+    }
+}
---
+++ src/BackTracking/NM2_15650.java
@@ -0,0 +1,4 @@
+package BackTracking;
+
+public class NM2_15650 {
+}
