3e32f165a6a18797483b09420e0877f7d4609ffa
최서연 <gkdltjdus@gmail.com>
2021-10-04 19:28:26 +0900

    2021.10.04/DP필수문제/2xN 타일링2(11727)

--- src/DynamicProgramming/BOJ11726.java
+++ src/DynamicProgramming/BOJ11726.java
@@ -4,22 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ1463 {
+public class BOJ11726 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 0;
+        // dp[2] = 2
+        int[] dp = new int[n + 2];
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + 1;
+        dp[1] = 1;
+        dp[2] = 2;
 
-            if (i % 3 == 0)
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-
-            if (i % 2 == 0)
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+        if(n >= 3) {
+            for(int i = 3; i <= n; i++)
+                dp[i] = (dp[i-2] + dp[i-1]) % 10007;
         }
         System.out.println(dp[n]);
         br.close();
--- src/DynamicProgramming/BOJ11727.java
+++ src/DynamicProgramming/BOJ11727.java
@@ -1,22 +1,21 @@
-package DynamicProgramming.Knapsack;
+package DynamicProgramming;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ11726 {
+public class BOJ11727 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        // dp[2] = 2
         int[] dp = new int[n + 2];
 
         dp[1] = 1;
-        dp[2] = 2;
+        dp[2] = 3;
 
         if(n >= 3) {
             for(int i = 3; i <= n; i++)
-                dp[i] = (dp[i-2] + dp[i-1]) % 10007;
+                dp[i] = (dp[i-1] + dp[i-2]*2) % 10007;
         }
         System.out.println(dp[n]);
         br.close();
