10e6ddc3d4d7408f83b4c3b67a86f3dc7f82a19e
최서연 <gkdltjdus@gmail.com>
2021-10-04 18:11:27 +0900

    2021.10.04/DP필수문제/피보나치 수열(2747)

---
+++ src/DynamicProgramming/BOJ2747.java
@@ -0,0 +1,27 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2747 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibo(n));
+    }
+
+    private static int fibo(int num) {
+        int[] dp = new int[num + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        if(num >= 2) {
+            for(int i = 2; i <= num; i++) {
+                dp[i] = dp[i - 2] + dp[i - 1];
+            }
+        }
+        return dp[num];
+    }
+}
