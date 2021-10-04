179e9d46b834e96306ec5aeac15e281ed6383fec
최서연 <gkdltjdus@gmail.com>
2021-10-04 19:53:14 +0900

    2021.10.04/DP필수문제/오르막수(11057)

---
+++ src/DynamicProgramming/BOJ11057.java
@@ -0,0 +1,18 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11057 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n + 1][10];
+
+
+
+
+        br.close();
+    }
+}
