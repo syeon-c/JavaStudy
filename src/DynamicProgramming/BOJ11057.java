92db0a07c13a735ec458b02f80a85ae285f7642f
최서연 <gkdltjdus@gmail.com>
2021-10-05 16:42:23 +0900

    2021.10.05/BOJ/DP필수문제/오르막수(11057)

--- src/DynamicProgramming/BOJ11057.java
+++ src/DynamicProgramming/BOJ11057.java
@@ -3,7 +3,6 @@ package DynamicProgramming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class BOJ11057 {
     public static void main(String[] args) throws IOException {
@@ -11,13 +10,16 @@ public class BOJ11057 {
         int n = Integer.parseInt(br.readLine());
         int[][] dp = new int[n + 1][10];
 
+        for(int i = 0; i < 10; i++)
+            dp[0][i] = 1;
+
         for(int i = 1; i <= n; i++) {
             dp[i][0] = 1;
             for(int j = 1; j < 10; j++)
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 10007;
         }
 
-        System.out.println(Arrays.stream(dp[n]).sum() % 10007);
+        System.out.println(dp[n][9]);
         br.close();
     }
 }
