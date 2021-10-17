aa827cc8e92b475ec87e81fab90df61dd26d21e4
최서연 <gkdltjdus@gmail.com>
2021-10-17 15:12:51 +0900

    2021.10.17/BOJ/DP필수문제/카드 구매하기(11052) 인덱스 오류 해결

--- src/DynamicProgramming/BOJ11052.java
+++ src/DynamicProgramming/BOJ11052.java
@@ -4,17 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * 카드는 카드팩의 형태로만 구매할 수 있고, 카드팩의 종류는 카드 1개가 포함된 카드팩, 카드 2개가 포함된 카드팩, ... 카드 N개가 포함된 카드팩과 같이 총 N가지가 존재한다.
- * 민규는 카드의 개수가 적은 팩이더라도 가격이 비싸면 높은 등급의 카드가 많이 들어있을 것이라는 미신을 믿고 있다.
- * 따라서, 민규는 돈을 최대한 많이 지불해서 카드 N개 구매하려고 한다.
- * 카드가 i개 포함된 카드팩의 가격은 Pi원이다.
- *
- * 카드 팩의 가격이 주어졌을 때, N개의 카드를 구매하기 위해 민규가 지불해야 하는 금액의 최댓값을 구하는 프로그램을 작성하시오.
- * N개보다 많은 개수의 카드를 산 다음, 나머지 카드를 버려서 N개를 만드는 것은 불가능하다.
- * 즉, 구매한 카드팩에 포함되어 있는 카드 개수의 합은 N과 같아야 한다.
- */
-
 public class BOJ11052 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,7 +20,7 @@ public class BOJ11052 {
 
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= i; j++) {
-                dp[i] = Math.max(dp[i - j] + p[j], dp[i]);
+                dp[i] = Math.max(dp[i], dp[i - j] + p[j]);
             }
         }
         System.out.println(dp[n]);
