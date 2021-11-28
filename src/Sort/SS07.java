11297f71e8645310afb2881ba1dfd43add4f157e
최서연 <gkdltjdus@gmail.com>
2021-11-28 17:43:06 +0900

    2021.11.28/Inflearn/Sorting&Searching/좌표 정렬(compareTo)

---
+++ src/Sort/SS07.java
@@ -0,0 +1,40 @@
+package Sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SS07 {
+    private static class Point implements Comparable<Point> {
+        int x; int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            if (this.x == o.x) return this.y - o.y;
+            else return this.x - o.x;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<Point> list = new ArrayList<>();
+        for(int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            int x = Integer.parseInt(input[0]);
+            int y = Integer.parseInt(input[1]);
+            list.add(new Point(x, y));
+        }
+        br.close();
+        Collections.sort(list);
+        for(Point p : list)
+            System.out.println(p.x + " " + p.y);
+    }
+}
