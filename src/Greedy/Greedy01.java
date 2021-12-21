f47a580c6d524c03253342f797af4576abb6ce66
최서연 <gkdltjdus@gmail.com>
2021-12-21 19:53:14 +0900

    Inflearn/Greedy/씨름 선수(시간 초과 문제)

--- src/Greedy/Greedy01.java
+++ src/Greedy/Greedy01.java
@@ -3,39 +3,50 @@ package Greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Greedy01 {
     private static int N;
     private static int[] W, H;
     private static int answer;
 
-    private static void Solution() {
-        for(int i = 0; i < N; i++) {
-            boolean flag = true;
-            int nh = H[i];
-            int nw = W[i];
-            for(int j = 0; j < N; j++) {
-                if (j == i) continue;
-                if (H[j] > nh && W[j] > nw) {
-                    flag = false;
-                    break;
-                }
+    private static class Info implements Comparable<Info>{
+        int height, weight;
+
+        public Info(int height, int weight) {
+            this.height = height;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Info o) {
+            return o.height - this.height;
+        }
+    }
+
+    private static void Solution(ArrayList<Info> arr) {
+        Collections.sort(arr);
+        int max = Integer.MIN_VALUE;
+        for(Info player : arr) {
+            if (max < player.weight) {
+                max = player.weight;
+                answer++;
             }
-            if (flag) answer++;
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        W = new int[N];
-        H = new int[N];
+        ArrayList<Info> arr = new ArrayList<>();
         for(int i = 0; i < N; i++) {
             String[] input = br.readLine().split(" ");
-            H[i] = Integer.parseInt(input[0]);
-            W[i] = Integer.parseInt(input[1]);
+            int h = Integer.parseInt(input[0]);
+            int w = Integer.parseInt(input[1]);
+            arr.add(new Info(h, w));
         }
         br.close();
-        Solution();
+        Solution(arr);
         System.out.println(answer);
     }
 }
