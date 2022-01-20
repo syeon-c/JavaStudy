2438df9ca710d7e748dd8f47b8db72354574f48a
최서연 <gkdltjdus@gmail.com>
2022-01-20 18:13:51 +0900

    2022.01.20/BOJ/Math/골드바흐의 추측(6588) > 에라토스테네스의 체

---
+++ src/Math/BOJ6588.java
@@ -0,0 +1,36 @@
+package Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ6588 {
+    private static final int MAX = 1000000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 소수인지 체크하는 배열 isPrime[] 생성 --> 에라토스테네스의 체
+        boolean[] isPrime = new boolean [MAX+1];
+        for(int i = 2; i <= MAX; i++)
+            isPrime[i] = true;
+        for(int i = 2; i <= MAX; i++) {
+            for(int j = i + i; j <= MAX; j += i) {
+                if(!isPrime[j]) continue;
+                isPrime[j] = false;
+            }
+        }
+        while (true) {
+            int N = Integer.parseInt(br.readLine());
+            boolean ok = false;
+            if (N == 0) break;
+            for(int i = 2; i <= N/2; i++) {
+                if(isPrime[i] && isPrime[N - i]) {
+                    System.out.println(N + " = " + i + " + " + (N-i));
+                    ok = true;
+                    break;
+                }
+            }
+            if (!ok) System.out.println("Goldbach's conjecture is wrong.");
+        }
+        br.close();
+    }
+}
