55a8e9594a6341b2527132060d5fe5007f9585bb
최서연 <gkdltjdus@gmail.com>
2022-01-21 15:37:41 +0900

    2022.01.21/BOJ/Math/GCD합(9613)

--- src/BOJ9613.java
+++ src/BOJ9613.java
@@ -2,12 +2,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class BOJ9613 {
-    public static int getGCD(int a, int b) {
-        while (b > 0) {
-            int tmp = b;
-            b = a % b;
-            a = tmp;
+    private static int getGCD(int a, int b) {
+        while(b > 0) {
+            int tmp = a;
+            a = b;
+            b = tmp % b;
         }
         return a;
     }
@@ -16,22 +19,23 @@ public class BOJ9613 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < n; i++) {
-            String[] numList = br.readLine().split(" ");
-            int size = Integer.parseInt(numList[0]);
-            int[] arr = new int[size];
+        while (n-- > 0) {
+            String[] input = br.readLine().split(" ");
+            int size = Integer.parseInt(input[0]);
 
-            for(int j = 0; j < size; j++) {
-                arr[j] = Integer.parseInt(numList[j + 1]);
-            }
+            int[] arr = new int[size];
+            for(int i = 0; i < size; i++)
+                arr[i] = Integer.parseInt(input[i + 1]);
+            Arrays.sort(arr);
 
             long sum = 0;
-            for(int z = 0; z < size - 1; z++) {
-                for(int x = z+1; x < size; x++) {
-                    sum += getGCD(arr[z], arr[x]);
+            for(int i = 0; i < size-1; i++) {
+                for(int j = i+1; j < size; j++) {
+                    sum += getGCD(arr[i], arr[j]);
                 }
             }
             System.out.println(sum);
         }
+        br.close();
     }
 }
