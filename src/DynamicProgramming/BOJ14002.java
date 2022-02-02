dd455d08eaed873fb2cbc88bccc6841abdd5a24f
최서연 <gkdltjdus@gmail.com>
2022-02-02 16:49:42 +0900

    2022.02.02/BOJ/DP/가장 긴 증가하는 부분 수열1, 4(11053, 14002)

--- src/DynamicProgramming/BOJ14002.java
+++ src/DynamicProgramming/BOJ14002.java
@@ -3,32 +3,39 @@ package DynamicProgramming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
-public class BOJ11053 {
+public class BOJ14002 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         String[] input = br.readLine().split(" ");
-        // dp[i]: arr[i] 마지막 항으로 하는 최대 증가 수열
-        int[] dp = new int[n];
-        int[] arr = new int[n];
-        int answer = 1;
+        int[] dp = new int[N];
+        int[] arr = new int[N];
 
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < N; i++)
             arr[i] = Integer.parseInt(input[i]);
-        br.close();
 
         dp[0] = 1;
-        for(int i = 1; i < n; i++) {
+        int answer = 1;
+        for(int i = 1; i < N; i++) {
             int cnt = 0;
             for(int j = i-1; j >= 0; j--) {
-                if(arr[i] > arr[j] && dp[j] > cnt)
-                    // arr[i]보다 작으면서 dp[j]가 최댓값인 경우
+                if (arr[i] > arr[j] && cnt < dp[j])
                     cnt = dp[j];
             }
             dp[i] = cnt + 1;
             answer = Math.max(answer, dp[i]);
         }
         System.out.println(answer);
+
+        Stack<Integer> list = new Stack<>();
+        for(int i = N - 1; i >= 0; i--) {
+            if (dp[i] == answer) {
+                list.push(arr[i]);
+                answer--;
+            }
+        }
+        while (!list.isEmpty()) System.out.print(list.pop() + " ");
     }
 }
