cecdc57b941cae300d6eed7a1765fa0ec3a76de6
Seoyeon <gkdltjdus@gmail.com>
2022-05-18 13:44:33 +0900

    프로그래머스/Lv2/행렬 곱셈

---
+++ src/Programmers/Level2/MatrixMultiplication.java
@@ -0,0 +1,28 @@
+package Programmers.Level2;
+
+public class MatrixMultiplication {
+    private static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for(int i = 0; i < arr1.length; i++) {
+            for(int j = 0; j < arr2[0].length; j++) {
+                for(int k = 0; k < arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[][] arr1 = {
+                {1, 4},
+                {3, 2},
+                {4, 1}
+        };
+        int[][] arr2 = {
+                {3, 3},
+                {3, 3}
+        };
+        solution(arr1, arr2);
+    }
+}
