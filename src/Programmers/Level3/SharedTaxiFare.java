e6a7f1239c55a7c694b9b4052cbbdc4e96f9bfbc
Seoyeon <gkdltjdus@gmail.com>
2022-06-26 17:29:11 +0900

    프로그래머스/Lv3/합승택시요금/플로이드-와샬(최단거리알고리즘)

--- src/Programmers/Level3/SharedTaxiFare.java
+++ src/Programmers/Level3/SharedTaxiFare.java
@@ -43,6 +43,6 @@ public class SharedTaxiFare {
         int a = 6;
         int b = 2;
         int[][] fares = {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}};
-        System.out.println(solution(n, s, a, b, fares));
+        solution(n, s, a, b, fares);
     }
 }
